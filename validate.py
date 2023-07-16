35e75b08f14710f73ba0d729b3647d46569980b1
Xubai Wang <18016038327@189.cn>
2023-07-16 10:30:20 +0800

    检查所有文件

--- validate.py
+++ validate.py
@@ -1,13 +1,19 @@
+from pathlib import Path
 from rdflib import Dataset
 
-path = "政治.ru"
-with open(path, 'r') as f:
-    update = f.read()
 
-print(update)
+def main():
+    directory = Path(__file__).parent
+    dataset = Dataset()
+    for p in directory.glob("*.ru"):
+        dataset.update(read_text(p))
+    for q in dataset:
+        print(q)
 
-d = Dataset()
-d.update(update)
 
-for s, p, o, g in d:
-    print(s, p, o, g)
+def read_text(path: Path) -> str:
+    with open(path, 'r') as f:
+        return f.read()
+
+
+__name__ == "__main__" and main()
